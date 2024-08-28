aaec7edcd89219c2be452d9a3929c10b8c3550cf
NewNorthStar <ghej@itu.dk>
2024-08-28 16:04:48 +0200

    Created a parser for Chirp messages.
    Co-authored-by: Lluc <llpa@itu.dk>
    Co-authored-by: Jakob <jakso@itu.dk>
    Co-authored-by: Jacob <spon@itu.dk>
    Co-authored-by: Ronas <rono@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Kan alle se projektet / Can all see the project"); 
+﻿using System.Collections;
+
+StreamReader reader = File.OpenText("/home/newnorthstar/Chirp.CLI/chirp_cli_db.csv");
+reader.ReadLine();
+string line;
+
+while ((line = reader.ReadLine()) != null)
+{
+    ArrayList list = new ArrayList(line.Split(','));
+    string username = (string) list[0];
+    list.RemoveAt(0);
+    string timecode = (string)list[list.Count - 1];
+    DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(int.Parse(timecode));
+    DateTime dateTime = dateTimeOffset.UtcDateTime;
+    timecode = dateTime.ToString();
+    list.RemoveAt(list.Count-1);
+    string message = "";
+    if (list.Count > 1)
+    {
+        while (list.Count != 0)
+        {
+            message = message + (string)list[0] + ",";
+            list.RemoveAt(0);
+        }
+
+        message = message.Substring(1, message.Length - 3);
+    }
+    else
+    {
+        message = (string)list[0];
+        message = message.Substring(1, message.Length - 2);
+    }
+    Console.WriteLine(username + " @ " + timecode + ": " + message);
+}
