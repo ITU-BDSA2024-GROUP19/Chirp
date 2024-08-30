1f0a156d3c43f8aea78958ad04b3bf4ec1c6746b
Jakob <jakso@itu.dk>
2024-08-30 18:18:10 +0200

    Implemented Regex for CSV parsing
    This was implemented for simplicity of our CSV parser, also ensuring that all cases that could arise in a CSV file is accounted for.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Chirp;
 
-public static class Program
+public static partial class Program
 {
     public static void Main(string[] args)
     {
@@ -22,11 +23,12 @@ public static class Program
 
                         break;
                     }
+                
                 case "cheep":
                     try
                     {
                         string cheep = args[1];
-                    } catch (IndexOutOfRangeException ex) { Console.WriteLine("Please enter a valid cheep");}
+                    } catch (IndexOutOfRangeException) { Console.WriteLine("Please enter a valid cheep");}
                     break;
                 default:
                     Console.WriteLine("Unknown Command");
@@ -38,14 +40,43 @@ public static class Program
 
     private static string CSVParser(string line)
     {
+        Regex CSVPattern = MyRegex();
+        string[] lines = CSVPattern.Split(line);
+        
+        string username = lines[0];
+        string message = lines[1].Substring(1, lines[1].Length - 2);
+        string timecode = lines[2];
+
+        timecode = TimecodeToCEST(timecode);
+        
+        return username + " @ " + timecode + ": " + message;
+    }
+
+    private static string TimecodeToCEST(string timecode)
+    {
+        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(int.Parse(timecode));
+        TimeZoneInfo danishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+        DateTime dateTime = TimeZoneInfo.ConvertTime(dateTimeOffset.UtcDateTime, danishTimeZone);
+        return dateTime.ToString(CultureInfo.InvariantCulture); // ensures the right format that is required
+    }
+
+    // Adapted from Stackoverflow: https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp/34265869#34265869
+    [GeneratedRegex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))")]
+    private static partial Regex MyRegex();
+    
+    
+    
+    
+    /*private static string CSVParser(string line)
+    {
         List<string> cheepFragments = line.Split(',').ToList();
         string username = cheepFragments[0];
         cheepFragments.RemoveAt(0);
-            
+
         string timecode = cheepFragments[^1]; //instead of cheepFragments.Count - 1, last element in list
         timecode = TimecodeToCEST(timecode);
         cheepFragments.RemoveAt(cheepFragments.Count-1);
-            
+
         string message = string.Join(',', cheepFragments);
         if (cheepFragments.Count > 1)
         {
@@ -58,12 +89,5 @@ public static class Program
         }
         return username + " @ " + timecode + ": " + message;
     }
-
-    private static string TimecodeToCEST(string timecode)
-    {
-        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(int.Parse(timecode));
-        TimeZoneInfo danishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
-        DateTime dateTime = TimeZoneInfo.ConvertTime(dateTimeOffset.UtcDateTime, danishTimeZone);
-        return dateTime.ToString(CultureInfo.InvariantCulture); // ensures the right format that is required
-    }
+    */
 }
