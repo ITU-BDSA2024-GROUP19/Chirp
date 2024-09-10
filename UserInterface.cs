4c8fa2458d93185e6062ea7769e78a85f019abb6
Jacob <spon@itu.dk>
2024-09-10 21:17:25 +0200

    Refactored elements of UI into UserInterface.cs
    This is done to improve readability of Program.cs especially if more components are added to the program further on, making Program.cs responsible for only running the program.
    
    Co-authored-by: Jakob <jakso@itu.dk>

---
+++ UserInterface.cs
@@ -0,0 +1,48 @@
+﻿namespace Chirp;
+
+public static class UserInterface
+{
+    public static String GetCLIMessage()
+    {
+        const string CLIMessage = @"Chirp CLI version.
+
+        Usage:
+            chirp read [<limit>]
+            chirp cheep <message>
+            chirp (-h | --help)
+            chirp --version
+
+        Options:
+            -h --help    Show this screen.
+            --version    Show version information.
+        ";
+        return CLIMessage;
+    }
+    
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (var record in cheeps)
+        {
+            Console.WriteLine(record.ToString());
+        }
+    }
+}
+
+public record Cheep(string Author, string Message, long Timestamp)
+{
+    /// <summary>
+    /// Builder method for new Cheeps.
+    /// </summary>
+    /// <returns>A Cheep with the message content. Includes the current time, and env. username.</returns>
+    public static Cheep NewCheep(string message) 
+    {
+        return new Cheep(Environment.UserName, message, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+    }
+
+    override public string ToString()
+    {
+        return Author + " @ " + Program.TimecodeToCEST(Timestamp) + ": " + Message;
+    }
+}
+
+
