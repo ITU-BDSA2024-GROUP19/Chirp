24930ba0fd3e9e44676b37329d3c9c0861df877d
Ronas Olsen <rono@itu.dk>
2024-09-16 09:19:08 +0200

    Test naming conventions.

---
+++ test/Program/Program_Unit.cs
@@ -0,0 +1,10 @@
+namespace Chirp.Test.Program;
+
+public class Program_Unit
+{
+    //[Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/SimpleDB/CSVDatabase_Integration.cs
@@ -0,0 +1,10 @@
+namespace Chirp.Test.SimpleDB;
+
+public class CSVDatabase_Integration
+{
+    //[Fact]
+    public void Test1()
+    {
+
+    }
+}
