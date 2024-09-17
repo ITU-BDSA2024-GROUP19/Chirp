7ff597253056488a7b5f71d2741f582def600f44
Jacob <spon@itu.dk>
2024-09-17 14:14:54 +0200

    First draft to the sington pattern for CSVDatabase.cs class

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -8,30 +8,45 @@ namespace Chirp.SimpleDB;
 /// Utilizes the CsvHelper library by Josh Close. Web: https://joshclose.github.io/CsvHelper/
 /// </summary>
 /// <typeparam name="T">the type of record in this database.</typeparam>
+/// Singleton pattern is based on C# in depth. Web: https://csharpindepth.com/Articles/Singleton
+
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    readonly string _path;
-
-    public CSVDatabase(string path) {
-        _path = path;
-    }
+    private static readonly Lazy<CSVDatabase<T>> lazy = new Lazy<CSVDatabase<T>>(() => new CSVDatabase<T>(_path));
+    private static string _path;
     
+    static CSVDatabase()
+    {
+        lazy = new Lazy<CSVDatabase<T>>(() => new CSVDatabase<T>(_path));
+    }
+
+    public static CSVDatabase<T> Instance(string path)
+    {
+        _path = path ?? throw new ArgumentNullException(nameof(path));
+        return lazy.Value;
+    }
+
+    private CSVDatabase(string path)
+    {
+        _path = path ?? throw new ArgumentNullException(nameof(path));
+    }
+
     public IEnumerable<T> Read(int? limit = null)
     {
-        using StreamReader sr = new (_path);
-        using CsvReader csv = new (sr, CultureInfo.InvariantCulture);
+        using StreamReader sr = new StreamReader(_path);
+        using CsvReader csv = new CsvReader(sr, CultureInfo.InvariantCulture);
         IEnumerable<T> records = csv.GetRecords<T>();
         if (limit != 0)
         {
-            List<T> results = new();
+            List<T> results = new List<T>();
             IEnumerator<T> iterator = records.GetEnumerator();
             for (int i = 0; i < limit && iterator.MoveNext(); i++)
             {
                 results.Add(iterator.Current);
             }
             return results;
-        } 
-        else 
+        }
+        else
         {
             return new List<T>(records);
         }
@@ -40,7 +55,7 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
     public void Store(T record)
     {
         using StreamWriter sw = File.AppendText(_path);
-        using CsvWriter csv = new (sw, CultureInfo.InvariantCulture);
+        using CsvWriter csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
         sw.WriteLine();
         csv.WriteRecord(record);
     }
