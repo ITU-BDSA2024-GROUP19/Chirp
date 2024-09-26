ee76ac4ffe169d868a91634d724c8a3cb04b23d0
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-09-26 14:34:49 +0200

    1a finished, added the environment variable for the db

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,41 +1,50 @@
 using System.Data;
-
 using Microsoft.Data.Sqlite;
+using SQLitePCL;
 
-namespace Chirp.Razor;
-
-public class DBFacade
+namespace Chirp.Razor
 {
-    public static List<CheepViewModel> Read()
+    public class DBFacade
     {
-        List<CheepViewModel> cheeps = new List<CheepViewModel>();
-        const string sqlDBFilePath = "data/chirp.db";
-        const string sqlQuery = @"SELECT u.username, m.text, m.pub_date FROM user u, message m WHERE u.user_id = m.author_id ORDER by message.pub_date desc";
-
-        using var connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = sqlQuery;
-
-        using var reader = command.ExecuteReader();
-        while (reader.Read())
+        public static List<CheepViewModel> Read()
         {
-            // See https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader.getvalues?view=dotnet-plat-ext-7.0
-            // for documentation on how to retrieve complete columns from query results
-            Object[] values = new Object[reader.FieldCount];
-            int fieldCount = reader.GetValues(values);
-            for (int i = 0; i < fieldCount; i += 3)
-                cheeps.Add(new CheepViewModel($"{values[i]}", $"{values[i+1]}", $"{UnixTimeStampToDateTimeString(Convert.ToDouble(values[i+2]))}"));
+            List<CheepViewModel> cheeps = new List<CheepViewModel>();
+            
+            // Initialize SQLite provider ChatGPT help
+            Batteries.Init();
+
+            string sqlDBFilePath = Environment.GetEnvironmentVariable("CHIRPDBPATH");
+            if (sqlDBFilePath == null)
+            {
+                sqlDBFilePath = "/tmp/chirp.db";
+            }
+            
+            string sqlQuery = @"SELECT u.username, m.text, m.pub_date FROM user u, message m WHERE u.user_id = m.author_id ORDER by m.pub_date desc";
+
+            using var connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = sqlQuery;
+
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Object[] values = new Object[reader.FieldCount];
+                    int fieldCount = reader.GetValues(values);
+                    for (int i = 0; i < fieldCount; i += 3)
+                        cheeps.Add(new CheepViewModel($"{values[i]}", $"{values[i+1]}", $"{UnixTimeStampToDateTimeString(Convert.ToDouble(values[i+2]))}"));
+                } 
+            }
+            return cheeps;
         }
-        return cheeps;
-    }
-    
-    private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
-    {
-        // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTimeStamp);
-        return dateTime.ToString("MM/dd/yy H:mm:ss");
+        
+        private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp);
+            return dateTime.ToString("MM/dd/yy H:mm:ss");
+        }
     }
 }
