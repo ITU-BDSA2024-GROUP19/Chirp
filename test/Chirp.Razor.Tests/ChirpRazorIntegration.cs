e3ebcf56d671284e5cb68cc18eaf365917eb4ca0
Jakob <jakso@itu.dk>
2024-09-29 20:16:16 +0200

    Created integration test for User timeline endpoint.
    Co-authored-by: Jacob <spon@itu.dk>

--- test/Chirp.Razor.Tests/ChirpRazorIntegration.cs
+++ test/Chirp.Razor.Tests/ChirpRazorIntegration.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 
+using Xunit.Abstractions;
+
 namespace Chirp.Razor.Tests;
 
 public class ChirpRazorIntegration : IClassFixture<WebApplicationFactory<Program>>
 {  
     private readonly WebApplicationFactory<Program> _fixture;
+    private readonly ITestOutputHelper _testOutputHelper;
     private readonly HttpClient _client;
 
-    public ChirpRazorIntegration(WebApplicationFactory<Program> fixture)
+    public ChirpRazorIntegration(WebApplicationFactory<Program> fixture, ITestOutputHelper testOutputHelper)
     {
         _fixture = fixture;
+        _testOutputHelper = testOutputHelper;
         _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
     }
     [Fact]
@@ -23,9 +27,17 @@ public class ChirpRazorIntegration : IClassFixture<WebApplicationFactory<Program
         Assert.Contains("Public Timeline", content);
     }
     
-    [Fact]
-    public void GETRequestToUserTimelineEndpoint()
+    [Theory]
+    [InlineData("Helge")]
+    [InlineData("Adrian")]
+    public async void GETRequestToUserTimelineEndpoint(string author)
     {
-        
+        var response = await _client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        _testOutputHelper.WriteLine(content);
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
     }
 }
