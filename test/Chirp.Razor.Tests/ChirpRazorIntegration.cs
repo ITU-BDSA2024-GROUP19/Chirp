6e6bbd370824230fdda28f0bcf34949c2c015944
Jakob <jakso@itu.dk>
2024-09-29 20:08:52 +0200

    Created integration test for public timeline endpoint.
    Co-authored-by: Jacob <spon@itu.dk>

--- test/Chirp.Razor.Tests/ChirpRazorIntegration.cs
+++ test/Chirp.Razor.Tests/ChirpRazorIntegration.cs
@@ -1,11 +1,30 @@
-using Chirp.Razor;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Chirp.Razor.Tests;
 
-public class ChirpRazorIntegration
-{
+public class ChirpRazorIntegration : IClassFixture<WebApplicationFactory<Program>>
+{  
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public ChirpRazorIntegration(WebApplicationFactory<Program> fixture)
+    {
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+    }
     [Fact]
-    public void Test1()
+    public async void GETRequestToPublicTimelineEndpoint()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+    
+    [Fact]
+    public void GETRequestToUserTimelineEndpoint()
     {
         
     }
