ad34233e721833a88f1c3d6498432b9a9fdd5669
Jakob <jakso@itu.dk>
2024-09-29 19:07:50 +0200

    Added test to check author, message, timestamp format.
    Co-authored-by: Jacob <spon@itu.dk>

--- test/Chirp.Razor.Tests/ChirpRazorUnit.cs
+++ test/Chirp.Razor.Tests/ChirpRazorUnit.cs
@@ -58,4 +58,21 @@ public class ChirpRazorUnit
         //Assert
         Assert.Equal(expectedUnixTime, returnedTime);
     }
+
+    [Fact]
+    public void FormatOfCheepsTest()
+    {
+        //Arrange
+        string expectedFormat1 = "Adrian Hej, velkommen til kurset. 01/08/2023 15:08:28";
+        string expectedFormat2 = "Helge Hello, BDSA students! 01/08/2023 14:16:48";
+
+        //Act
+        List<CheepViewModel> cheeps = DBFacade.Read(1);
+        string cheepFormat1 = cheeps[0].Author + " " + cheeps[0].Message + " " + cheeps[0].Timestamp;
+        string cheepFormat2 = cheeps[1].Author + " " + cheeps[1].Message + " " + cheeps[1].Timestamp;
+
+        //Assert
+        Assert.Equal(expectedFormat1, cheepFormat1);
+        Assert.Equal(expectedFormat2, cheepFormat2);
+    }
 }
