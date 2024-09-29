9d232ff5a033ccadb2c7a4b93b80c9a064c99b8e
Jakob <jakso@itu.dk>
2024-09-29 18:34:33 +0200

    Added unit test for specified author.
    Co-authored-by: Jacob <spon@itu.dk>

--- test/Chirp.Razor.Tests/ChirpRazorUnit.cs
+++ test/Chirp.Razor.Tests/ChirpRazorUnit.cs
@@ -4,9 +4,18 @@ namespace Chirp.Razor.Tests;
 
 public class ChirpRazorUnit
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("Helge")]
+    [InlineData("Adrian")]
+    public void ReadSpecifiedAuthorsCheepsTest(string author)
     {
+        //Arrange
+        int expectedNumberOfCheeps = 1;
         
+        //Act
+        List<CheepViewModel> cheepsFromAuthor = DBFacade.UserRead(author, 1);
+        
+        //Assert
+        Assert.Equal(expectedNumberOfCheeps, cheepsFromAuthor.Count);
     }
 }
