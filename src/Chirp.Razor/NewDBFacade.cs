18019df977241b57762489c6148b84f96b30eeca
Jacob <spon@itu.dk>
2024-10-01 16:14:34 +0200

    Made NewDBFacade.cs to work as the original DBFacade with the singleton concept.
    Co-authored-by: Ronas Olsen <rono@itu.dk>

--- src/Chirp.Razor/NewDBFacade.cs
+++ src/Chirp.Razor/NewDBFacade.cs
@@ -1,20 +1,60 @@
 using System.Globalization;
-
 using Microsoft.Data.Sqlite;
 using SQLitePCL;
 
 namespace Chirp.Razor;
 
-class NewDBFacade : ICheepService
+class NewDBFacade : ICheepService, IDisposable
 {
+    private const int CHEEPS_PER_PAGE = 32;
+    private readonly SqliteConnection Connection;
+
+    public NewDBFacade()
+    {
+        // Call to SQLitePCL.Batteries.Init() initializes the necessary provider for SQLite before attempting to open a connection to the database.
+        // Information retrieved from ChatGPT.
+        //Batteries.Init();
+        string sqlDBFilePath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? Path.GetTempPath() + "chirp.db";
+        Connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
+    }
+    
     public List<CheepViewModel> GetCheeps(int page)
     {
-        throw new NotImplementedException();
+        List<CheepViewModel> cheeps = new List<CheepViewModel>();
+        var command = Connection.CreateCommand();
+        command.CommandText = @"SELECT u.username, m.text, m.pub_date FROM user u, message m WHERE u.user_id = m.author_id ORDER by m.pub_date desc LIMIT {CHEEPS_PER_PAGE} OFFSET " + getPageOffset(page);
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Object[] values = new Object[reader.FieldCount];
+            int fieldCount = reader.GetValues(values);
+            for (int i = 0; i < fieldCount; i += 3)
+                cheeps.Add(new CheepViewModel($"{values[i]}", $"{values[i+1]}", $"{UnixTimeStampToDateTimeString(Convert.ToInt64(values[i+2]))}"));
+        }
+        return cheeps;
     }
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author, int page)
     {
-        throw new NotImplementedException();
+        List<CheepViewModel> cheeps = new List<CheepViewModel>();
+        var command = Connection.CreateCommand();
+        command.CommandText = $"SELECT u.username, m.text, m.pub_date FROM user u, message m WHERE u.user_id = m.author_id AND u.username = '{author}' ORDER by m.pub_date desc LIMIT {{CHEEPS_PER_PAGE}} OFFSET " + getPageOffset(page);
+        
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Object[] values = new Object[reader.FieldCount];
+            int fieldCount = reader.GetValues(values);
+            for (int i = 0; i < fieldCount; i += 3)
+                cheeps.Add(new CheepViewModel($"{values[i]}", $"{values[i+1]}", $"{UnixTimeStampToDateTimeString(Convert.ToInt64(values[i+2]))}"));
+        }
+        return cheeps;
+    }
+
+    public void Dispose()
+    {
+        Connection.Dispose();
     }
 
     public static string UnixTimeStampToDateTimeString(long unixTimeStamp)
@@ -23,5 +63,10 @@ class NewDBFacade : ICheepService
         TimeZoneInfo danishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
         DateTime dateTime = TimeZoneInfo.ConvertTime(dateTimeOffset.UtcDateTime, danishTimeZone);
         return dateTime.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+    }
+
+    private static int getPageOffset(int page)
+    {
+        return (page - 1) * CHEEPS_PER_PAGE;
     }
 }
