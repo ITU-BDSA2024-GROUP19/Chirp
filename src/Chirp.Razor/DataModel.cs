290ebb2045be30b177bbf29140252e0cb01e9ba6
ghej <ghej@itu.dk>
2024-10-02 13:42:42 +0200

    Began implementation of Data Model

---
+++ src/Chirp.Razor/DataModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+public class Cheep {
+    [Key]
+    public string Text {get; set;}
+    public DateTime TimeStamp {get; set;}
+    public Author Author {get; set;}
+}
+
+public class Author {
+    [Key]
+    public string Name {get; set;}
+    public string Email {get; set;}
+    public ICollection<Cheep> Cheeps {get; set;}
+}
+
+public class ChatDBContext : DbContext  {
+    public DbSet<Cheep> Cheeps {get; set;}
+    public DbSet<Author> Authors {get; set;}
+    public ChatDBContext (DbContextOptions<ChatDBContext> options) : base(options){
+    }
+}
