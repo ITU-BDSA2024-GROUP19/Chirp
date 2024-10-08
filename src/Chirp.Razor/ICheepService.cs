bff0ce1a92e29a75b155472291ddea0dddc4da74
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-10-08 15:05:20 +0200

    delete DBFacade.cs and ICheepService.cs
    Co-authored-by: Ronas Olsen <rono@itu.dk>
    Co-authored-by: Jakob <jakso@itu.dk>

--- src/Chirp.Razor/ICheepService.cs
+++
@@ -1,7 +0,0 @@
-public record CheepViewModel(string Author, string Message, string Timestamp);
-
-public interface ICheepService
-{
-    public List<CheepViewModel> GetCheeps(int page);
-    public List<CheepViewModel> GetCheepsFromAuthor(string author, int page);
-}
