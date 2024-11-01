ba11ab8c68b812c0f05cfdd26e49ec22733b7b83
Ronas Olsen <rono@itu.dk>
2024-11-01 16:40:06 +0100

    Modified Program.cs to use Startup class.

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using Chirp.Infrastructure;
 using Chirp.Core;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using InvalidOperationException = System.InvalidOperationException;
 
 namespace Chirp.Web;
@@ -11,62 +10,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        string dbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? throw new InvalidOperationException("You must specify a environment variable CHIRPDBPATH, use eg. $env:CHIRPDBPATH=C:/Temp/db.db");
+        string connectionString = "Data Source=" + dbPath;
+        
         var builder = WebApplication.CreateBuilder(args);
 
-        string ChirpDBPath = Environment.GetEnvironmentVariable("CHIRPDBPATH") ?? throw new InvalidOperationException("You must specify a environment variable CHIRPDBPATH, use eg. $env:CHIRPDBPATH=C:/Temp/db.db");
-        string connectionString = "Data Source=" + ChirpDBPath;
-        builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
-        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-        builder.Services.AddScoped<ICheepService, CheepService>();
-        builder.Services.AddRouting();
-        builder.Services.AddAuthentication(options => 
-        {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        })
-        .AddCookie()
-        .AddGitHub(o => 
-        {
-            o.ClientId = builder.Configuration["authentication:github:clientId"] ?? throw new InvalidOperationException("You must provide a github clientId");
-            o.ClientSecret = builder.Configuration["authentication:github:clientSecret"] ?? throw new InvalidOperationException("You must provide a github clientSecret");
-            o.CallbackPath = "/signin-github";
-        });
+        var startup = new Startup(builder.Configuration, connectionString);
 
-        builder.Services.AddDefaultIdentity<Author>(options => 
-        {
-            options.SignIn.RequireConfirmedAccount = true;
-        })
-            .AddEntityFrameworkStores<ChirpDBContext>();
-        
-        builder.Services.AddRazorPages();
+        startup.ConfigureServices(builder.Services);
 
         var app = builder.Build();
     
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        // Database currently migrated and seeded on every start. 
-        // Remove when data needs to be persistent on server. 
-        using (var scope = app.Services.CreateScope())
-        {
-            using var context = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
-            using var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Author>>();
-            context.Database.Migrate();
-            DbInitializer.SeedDatabase(context, userManager);
-        }
-
-        app.UseDeveloperExceptionPage();
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapRazorPages();
+        startup.Configure(app);
 
         app.Run();
     }
