da7b057e70a062ed67c1d972e13d6ac5b65eab2d
Ronas Olsen <rono@itu.dk>
2024-11-02 19:31:10 +0100

    Added test arrangement.

--- test/Chirp.Web.Test/AccountIntegrationTests.cs
+++ test/Chirp.Web.Test/AccountIntegrationTests.cs
@@ -1,8 +1,42 @@
 using Xunit;
+using Microsoft.Data.Sqlite;
+using Microsoft.AspNetCore.Builder;
+using Chirp.Web.Areas.Identity.Pages.Account;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using Chirp.Core;
+
+namespace Chirp.Web.Test;
 
 public class AccountIntegrationTests {
-    [Fact]
-    void OnMissingAccount_GivesErrorMessage() {
+    private static WebApplication PrepareTestApplication() {
+        var builder = WebApplication.CreateBuilder();
+
+        using var dbConn = new SqliteConnection("Data Source=:memory:");
+        dbConn.Open();
+
+        var startup = new Startup(builder.Configuration, dbConn);
+
+        startup.ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        startup.Configure(app);
         
+        return app;
+    }
+
+    [Fact]
+    public void OnMissingAccount_HandlesDatabaseMissCorrectly() {
+        // Arrange
+        using var app = PrepareTestApplication();
+        using var scope = app.Services.CreateScope();
+
+        var signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<Author>>();
+        using var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Author>>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<LoginModel>>();
+
+        var login_cs = new LoginModel(signInManager, logger, userManager);
     }
 }
