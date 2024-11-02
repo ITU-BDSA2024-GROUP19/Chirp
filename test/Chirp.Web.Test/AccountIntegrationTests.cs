0035dcb30f7f5ab916093cfacc23af02b1f27967
Ronas Olsen <rono@itu.dk>
2024-11-02 21:18:47 +0100

    Created AppTestFixture tp limit using statements in each test.

--- test/Chirp.Web.Test/AccountIntegrationTests.cs
+++ test/Chirp.Web.Test/AccountIntegrationTests.cs
@@ -1,40 +1,20 @@
 using Xunit;
-using Microsoft.Data.Sqlite;
-using Microsoft.AspNetCore.Builder;
 using Chirp.Web.Areas.Identity.Pages.Account;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 using Chirp.Core;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Http;
 
 namespace Chirp.Web.Test;
 
-public class AccountIntegrationTests {
-    private static WebApplication PrepareTestApplication(SqliteConnection dbConn) 
-    {
-        var builder = WebApplication.CreateBuilder();
-
-        var startup = new Startup(builder.Configuration, dbConn);
-
-        startup.ConfigureServices(builder.Services);
-
-        var app = builder.Build();
-
-        startup.Configure(app);
-        
-        return app;
-    }
-
+public class AccountIntegrationTests 
+{
     [Fact]
     public async Task OnMissingAccount_NoThrowOnUserDatabaseMiss() 
     {
         // Arrange
-        using var dbConn = new SqliteConnection("Data Source=:memory:");
-        dbConn.Open();
-        using var app = PrepareTestApplication(dbConn);
-        using var scope = app.Services.CreateScope();
+        using var fixture = new AppTestFixture();
+        using var scope = fixture.App.Services.CreateScope();
 
         var signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<Author>>();
         using var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Author>>();
