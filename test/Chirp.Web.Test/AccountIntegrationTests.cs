81f253bcb34de68c5bebe7d5b952df8b7ee24455
Ronas Olsen <rono@itu.dk>
2024-11-02 20:58:04 +0100

    Fixes test failure due to database closing.

--- test/Chirp.Web.Test/AccountIntegrationTests.cs
+++ test/Chirp.Web.Test/AccountIntegrationTests.cs
@@ -6,15 +6,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 using Chirp.Core;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Http;
 
 namespace Chirp.Web.Test;
 
 public class AccountIntegrationTests {
-    private static WebApplication PrepareTestApplication() {
+    private static WebApplication PrepareTestApplication(SqliteConnection dbConn) 
+    {
         var builder = WebApplication.CreateBuilder();
-
-        using var dbConn = new SqliteConnection("Data Source=:memory:");
-        dbConn.Open();
 
         var startup = new Startup(builder.Configuration, dbConn);
 
@@ -28,9 +28,12 @@ public class AccountIntegrationTests {
     }
 
     [Fact]
-    public async Task OnMissingAccount_NoThrowOnUserDatabaseMiss() {
+    public async Task OnMissingAccount_NoThrowOnUserDatabaseMiss() 
+    {
         // Arrange
-        using var app = PrepareTestApplication();
+        using var dbConn = new SqliteConnection("Data Source=:memory:");
+        dbConn.Open();
+        using var app = PrepareTestApplication(dbConn);
         using var scope = app.Services.CreateScope();
 
         var signInManager = scope.ServiceProvider.GetRequiredService<SignInManager<Author>>();
@@ -38,10 +41,13 @@ public class AccountIntegrationTests {
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<LoginModel>>();
 
         var login_cs = new LoginModel(signInManager, logger, userManager);
-        login_cs.Input.Email = "strangeuser@example.com";
+        login_cs.Input = new()
+        {
+            Email = "strangeuser@example.com"
+        };
 
         // Act
-        await login_cs.OnPostAsync();
+        await login_cs.OnPostAsync("/");
 
         // Assert
         // Success if completed without an exception.
