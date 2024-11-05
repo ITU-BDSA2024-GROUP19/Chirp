e86b809fe257cb64c8dc7c9539a6af4943963efa
ghej <ghej@itu.dk>
2024-11-05 15:22:04 +0100

    Created methods prepareContents and OnPostAsync
    Co-Authored-By: RonoITU <151834029+RonoITU@users.noreply.github.com>
    Co-Authored-By: Jakob Sønder <161822463+jakso535@users.noreply.github.com>
    Co-Authored-By: llucitu <179705591+llucitu@users.noreply.github.com>

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -2,28 +2,63 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Infrastructure;
 using Chirp.Web.Pages.Shared.Models;
+using Microsoft.AspNetCore.Identity;
+using Chirp.Core;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 
 namespace Chirp.Web.Pages;
 
 public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
+
+    private readonly SignInManager<Author> _signInManager;
+
     public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
 
     [BindProperty]
     public CheepFormModel Input { get; set; } = new();
     
     public int CurrentPage { get; set; }
-    public PublicModel(ICheepService service)
+
+
+    public PublicModel(ICheepService service, SignInManager<Author> signInManager)
     {
         _service = service;
+        _signInManager = signInManager;
     }
 
-    public ActionResult OnGet()
+
+
+    private void prepareContents()
     {
         var pageQuery = Request.Query["page"];
         CurrentPage = Convert.ToInt32(pageQuery) == 0 ? 1 : Convert.ToInt32(pageQuery);
         Cheeps = _service.GetCheeps(CurrentPage);
+    }
+
+
+    public ActionResult OnGet()
+    {
+        prepareContents();
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (ModelState.IsValid)
+        {
+            Author ?author = await _signInManager.UserManager.GetUserAsync(User);
+            if (author == null)
+            {
+                return Forbid("You are not logged in!!!");
+            }
+            _service.AddCheep(author, Input.Message);
+        }
+        prepareContents();
+
+
         return Page();
     }
 }
