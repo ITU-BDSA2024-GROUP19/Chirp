a9b0262a7cadb1eab2e6f9dddd5213d433b53ca1
Jakob <jakso@itu.dk>
2024-11-13 17:03:12 +0100

    Added necessary dependency injections to ChirpAccountRepository.cs
    Co-authored-by: Ronas Olsen <rono@itu.dk>
    Co-authored-by: Jacob <spon@itu.dk>

--- src/Chirp.Infrastructure/ChirpAccountRepository.cs
+++ src/Chirp.Infrastructure/ChirpAccountRepository.cs
@@ -1,5 +1,10 @@
 using Chirp.Core;
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.Extensions.Logging;
+
 namespace Chirp.Infrastructure;
 
 public interface IChirpAccountRepository
@@ -11,14 +16,45 @@ public class ChirpAccountRepository : IChirpAccountRepository
 {
     private readonly ChirpDBContext _dbContext;
     
-    public ChirpAccountRepository(ChirpDBContext dbContext)
+    private readonly IChirpAccountRepository _repository;
+    private readonly SignInManager<Author> _signInManager;
+    private readonly UserManager<Author> _userManager;
+    private readonly IUserStore<Author> _userStore;
+    private readonly IUserEmailStore<Author> _emailStore;
+    private readonly ILogger<RegisterModel> _logger;
+    private readonly IEmailSender _emailSender;
+
+    public ChirpAccountRepository (
+        ChirpDBContext dbContext,
+        IChirpAccountRepository repository,
+        UserManager<Author> userManager,
+        IUserStore<Author> userStore,
+        SignInManager<Author> signInManager,
+        ILogger<RegisterModel> logger,
+        IEmailSender emailSender)
     {
         _dbContext = dbContext;
+        _repository = repository;
+        _userManager = userManager;
+        _userStore = userStore;
+        _emailStore = GetEmailStore();
+        _signInManager = signInManager;
+        _logger = logger;
+        _emailSender = emailSender;
     }
     
     public async Task AddAuthor(Author author)
     {
         _dbContext.Add(author);
         await _dbContext.SaveChangesAsync();
+    }
+    
+    private IUserEmailStore<Author> GetEmailStore()
+    {
+        if (!_userManager.SupportsUserEmail)
+        {
+            throw new NotSupportedException("The default UI requires a user store with email support.");
+        }
+        return (IUserEmailStore<Author>)_userStore;
     }
 }
