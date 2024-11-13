76070790a9aaa242794b7d65ecfa0cb760d18fba
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-11-13 15:54:19 +0100

    Add a list in Author of the people they are following

--- src/Chirp.Core/EntityClasses.cs
+++ src/Chirp.Core/EntityClasses.cs
@@ -15,4 +15,6 @@ public class Cheep {
 
 public class Author : IdentityUser {
     public required ICollection<Cheep> Cheeps {get; set;}
+    
+    public required ICollection<Author> Following {get; set;}
 }
