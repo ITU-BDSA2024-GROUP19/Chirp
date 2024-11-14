4530c834db7ba8e7656499cb55cf450d53b0b093
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-11-14 12:31:55 +0100

    Add the atribute is_followed to the cheepDTO

--- src/Chirp.Core/DataTransferObjects.cs
+++ src/Chirp.Core/DataTransferObjects.cs
@@ -1,3 +1,3 @@
 namespace Chirp.Core;
 
-public record CheepDTO(string Author, string Message, long Timestamp);
+public record CheepDTO(string Author, string Message, long Timestamp, bool is_followed);
