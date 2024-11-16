9894043224125b21fa6901f3e356d65ec59d58ba
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-11-16 18:31:13 +0100

    fix test and stub to  compile

--- test/Chirp.Infrastructure.Test/Stub/CheepRepositoryStub.cs
+++ test/Chirp.Infrastructure.Test/Stub/CheepRepositoryStub.cs
@@ -46,6 +46,17 @@ public class CheepRepositoryStub : ICheepRepository
         
         return Task.FromResult(mockCheeps);
     }
+    public Task<List<CheepDTO>> GetCheepDTOFromMe(int page, string userName)
+    {
+        var mockCheeps = new List<CheepDTO>
+        {
+            new CheepDTO("Author1", "This is a mock cheep 1", 1634567890, true),
+            new CheepDTO("Author2", "This is a mock cheep 2", 1634567900, true),
+            new CheepDTO("Author3", "This is a mock cheep 3", 1634567910, true)
+        };
+        
+        return Task.FromResult(mockCheeps);
+    }
     public Task<Author> GetAuthorByName(string name)
     {
         var mockAuthor = new Author() { UserName = "Author1", Email = "au1@itu.dk", Cheeps = new List<Cheep>(), Following = new List<Author>(), Followers = new List<Author>() };
@@ -57,7 +68,11 @@ public class CheepRepositoryStub : ICheepRepository
         return Task.FromResult(mockAuthor);
     }
     
-    public Task FollowAndUnfollowAuthor(string followerName, string authorName)
+    public Task FollowAuthor(string followerName, string authorName)
+    {
+        return Task.CompletedTask;
+    }
+    public Task UnfollowAuthor(string followerName, string authorName)
     {
         return Task.CompletedTask;
     }
