cb8e6e3bf05370c4487084f747606e8eb4b228e8
Ronas Olsen <rono@itu.dk>
2024-11-30 14:15:36 +0100

    DbInitializer utilizes EF core tracking capabilities.

--- src/Chirp.Core/EntityClasses.cs
+++ src/Chirp.Core/EntityClasses.cs
@@ -10,7 +10,9 @@ public class Cheep
     [Required]
     [StringLength(160)]
     public required string Text { get; set; }
+    [Required]
     public required DateTime TimeStamp { get; set; }
+    [Required]
     public required Author Author { get; set; }
 }
 
@@ -18,5 +20,10 @@ public class Author : IdentityUser
 {
     public ICollection<Cheep> Cheeps { get; set; } = new List<Cheep>();
     public ICollection<Author> Following { get; set; } = new List<Author>();
-    public ICollection<Author> Followers {get; set;} = new List<Author>();
+    public ICollection<Author> Followers { get; set ;} = new List<Author>();
+
+    public void AddSampleCheep(string text, DateTime date) 
+    {
+        Cheeps.Add(new Cheep() { Author = this, Text = text, TimeStamp = date });
+    }
 }
