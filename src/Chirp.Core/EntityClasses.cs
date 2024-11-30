cd7ef6b3ce773705d2b1a2d367d53b44a96199c9
Ronas Olsen <rono@itu.dk>
2024-11-30 13:45:51 +0100

    Author collections default to empty lists.

--- src/Chirp.Core/EntityClasses.cs
+++ src/Chirp.Core/EntityClasses.cs
@@ -9,14 +9,14 @@ public class Cheep
     public int CheepId { get; set; }
     [Required]
     [StringLength(160)]
-    public required string Text {get; set;}
-    public required DateTime TimeStamp {get; set;}
-    public required Author Author {get; set;}
+    public required string Text { get; set; }
+    public required DateTime TimeStamp { get; set; }
+    public required Author Author { get; set; }
 }
 
 public class Author : IdentityUser 
 {
-    public required ICollection<Cheep> Cheeps {get; set;}
+    public ICollection<Cheep> Cheeps { get; set; } = new List<Cheep>();
     public ICollection<Author> Following { get; set; } = new List<Author>();
     public ICollection<Author> Followers {get; set;} = new List<Author>();
 }
