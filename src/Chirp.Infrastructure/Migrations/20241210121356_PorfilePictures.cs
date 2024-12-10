3276ed9234d0e75b078c52a6eaf0dbcf9ae6e743
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-12-10 13:28:26 +0100

    merge with like fiture

---
+++ src/Chirp.Infrastructure/Migrations/20241210121356_PorfilePictures.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PorfilePictures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+        }
+    }
+}
