9245fe46020b9164a8789c61f644430b3b721b02
Jacob <spon@itu.dk>
2024-12-14 15:04:34 +0100

    Added the new interface IBlobServiceClient.cs
    Co-authored-by: Ronas Olsen <rono@itu.dk>

---
+++ src/Chirp.Infrastructure/External/IBlobServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Chirp.Infrastructure.External;
+
+public interface IBlobServiceClient
+{
+    BlobContainerClient GetBlobContainerClient(string BlobContainerName);
+}
+
+public class AzureBlobServiceClient : IBlobServiceClient
+{
+    private readonly BlobServiceClient? _client;
+
+    public AzureBlobServiceClient(BlobServiceClient? blobServiceClient = null)
+    {
+        _client = blobServiceClient;
+    }
+
+    public BlobContainerClient GetBlobContainerClient(string blobContainerName)
+    {
+        if (_client != null) 
+        {
+            return _client.GetBlobContainerClient(blobContainerName);
+        }
+        else
+        {
+            throw new InvalidOperationException("Azure Blob Service is unavailable");
+        }
+    }
+}
